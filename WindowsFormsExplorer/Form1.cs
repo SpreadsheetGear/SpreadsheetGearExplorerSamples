f81b1094fc826d5e2703fc2f752033e98d890224
Chris <chris@tracktownsoftware.com>
2021-05-18 10:14:38 -0700

    Didn't get commited and pushed on previous attempt

--- WindowsFormsExplorer/Form1.cs
+++ WindowsFormsExplorer/Form1.cs
@@ -4,6 +4,10 @@
 * SpreadsheetGear® is a registered trademark of SpreadsheetGear LLC.
 */
 
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
 using System.Windows.Forms;
 using SharedSamples;
 
@@ -19,9 +23,27 @@ namespace WindowsFormsExplorer
 
         private void Form1_Load(object sender, System.EventArgs e)
         {
+            InitializeTreeviewImages();
             PopulateSampleTree();
         }
 
+        private ImageList _samplesImageList;
+        // index values below are image position in _samplesImageList
+        private const int EngineImageIndex = 2; // brackets_curly_32
+        private const int WindowsImageIndex = 3; // window_alt_32
+
+        private void InitializeTreeviewImages()
+        {
+            _samplesImageList = new ImageList();
+            _samplesImageList.Images.Add(new Bitmap(WindowsFormsExplorer.Properties.Resources.FolderOpened_32));
+            _samplesImageList.Images.Add(new Bitmap(WindowsFormsExplorer.Properties.Resources.FolderClosed_32));
+            _samplesImageList.Images.Add(new Bitmap(WindowsFormsExplorer.Properties.Resources.brackets_curly_32));
+            _samplesImageList.Images.Add(new Bitmap(WindowsFormsExplorer.Properties.Resources.window_alt_32));
+            _samplesImageList.ImageSize = new Size(32, 32); // increase size from 16x16 deafault
+            samplesTreeView.ImageList = _samplesImageList;
+            samplesTreeView.AfterCollapse += SamplesTreeView_AfterCollapse;
+            samplesTreeView.AfterExpand += SamplesTreeView_AfterExpand;
+        }
 
         private void PopulateSampleTree()
         {
@@ -42,7 +64,7 @@ namespace WindowsFormsExplorer
 
         private void BuildTreeView(TreeNode currentNode, Category currentCategory)
         {
-            var categoryNode = new TreeNode(currentCategory.Name);
+            var categoryNode = new TreeNode(currentCategory.Name, 0, 0);
             categoryNode.Tag = currentCategory;
             if (currentNode != null)
                 currentNode.Nodes.Add(categoryNode);
@@ -50,7 +72,9 @@ namespace WindowsFormsExplorer
                 samplesTreeView.Nodes.Add(categoryNode);
             foreach (var sampleInfo in currentCategory.SampleInfos)
             {
-                var sampleNode = new TreeNode(sampleInfo.Name);
+                int imageIndex = sampleInfo.IsSharedEngineSample ? EngineImageIndex : WindowsImageIndex;
+                int selectedImageIndex = imageIndex;
+                var sampleNode = new TreeNode(sampleInfo.Name, imageIndex, selectedImageIndex);
                 sampleNode.Tag = sampleInfo;
                 categoryNode.Nodes.Add(sampleNode);
             }
@@ -59,7 +83,6 @@ namespace WindowsFormsExplorer
                 BuildTreeView(categoryNode, childCategory);
             }
         }
-
 
         private void samplesTreeView_AfterSelect(object sender, TreeViewEventArgs e)
         {
@@ -84,17 +107,28 @@ namespace WindowsFormsExplorer
             }
         }
 
-
         private void button_expand_Click(object sender, System.EventArgs e)
         {
             samplesTreeView.ExpandAll();
         }
-
 
         private void button_collapse_Click(object sender, System.EventArgs e)
         {
             samplesTreeView.CollapseAll();
             samplesTreeView.Nodes[0].Expand();
         }
+
+        private void SamplesTreeView_AfterExpand(object sender, TreeViewEventArgs e)
+        {
+            e.Node.ImageIndex = 0;
+            e.Node.SelectedImageIndex = 0;
+        }
+
+        private void SamplesTreeView_AfterCollapse(object sender, TreeViewEventArgs e)
+        {
+            e.Node.ImageIndex = 1;
+            e.Node.SelectedImageIndex = 1;
+        }
+
     }
 }
