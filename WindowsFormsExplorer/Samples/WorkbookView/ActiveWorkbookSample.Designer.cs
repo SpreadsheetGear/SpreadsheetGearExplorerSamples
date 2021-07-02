d8ed8d5c1aa0262da86fcdddbc26d1faf33d165d
Tim Andersen <tim@spreadsheetgear.com>
2021-07-02 16:16:09 -0500

    Tweak / fix ActiveWorkbookSample label for current workbok loaded

--- WindowsFormsExplorer/Samples/WorkbookView/ActiveWorkbookSample.Designer.cs
+++ WindowsFormsExplorer/Samples/WorkbookView/ActiveWorkbookSample.Designer.cs
@@ -54,16 +54,16 @@ namespace WindowsFormsExplorer.Samples.WorkbookView
             this.tableLayoutPanel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.tableLayoutPanel1.ColumnCount = 2;
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 300F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 429F));
             this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 0);
             this.tableLayoutPanel1.Controls.Add(this.panel2, 1, 0);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
-            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 1;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(551, 323);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(787, 538);
             this.tableLayoutPanel1.TabIndex = 0;
             // 
             // panel1
@@ -73,16 +73,18 @@ namespace WindowsFormsExplorer.Samples.WorkbookView
             this.panel1.Controls.Add(this.buttonNewWorkbook);
             this.panel1.Controls.Add(this.buttonLoadDisk);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Location = new System.Drawing.Point(4, 5);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(245, 317);
+            this.panel1.Size = new System.Drawing.Size(350, 528);
             this.panel1.TabIndex = 2;
             // 
             // buttonLoadUriXLSX
             // 
-            this.buttonLoadUriXLSX.Location = new System.Drawing.Point(11, 125);
+            this.buttonLoadUriXLSX.Location = new System.Drawing.Point(16, 208);
+            this.buttonLoadUriXLSX.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.buttonLoadUriXLSX.Name = "buttonLoadUriXLSX";
-            this.buttonLoadUriXLSX.Size = new System.Drawing.Size(221, 23);
+            this.buttonLoadUriXLSX.Size = new System.Drawing.Size(316, 38);
             this.buttonLoadUriXLSX.TabIndex = 3;
             this.buttonLoadUriXLSX.Text = "Load from URI (File on Server)";
             this.buttonLoadUriXLSX.UseVisualStyleBackColor = true;
@@ -90,9 +92,10 @@ namespace WindowsFormsExplorer.Samples.WorkbookView
             // 
             // buttonLoadUriAsp
             // 
-            this.buttonLoadUriAsp.Location = new System.Drawing.Point(12, 96);
+            this.buttonLoadUriAsp.Location = new System.Drawing.Point(17, 160);
+            this.buttonLoadUriAsp.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.buttonLoadUriAsp.Name = "buttonLoadUriAsp";
-            this.buttonLoadUriAsp.Size = new System.Drawing.Size(220, 23);
+            this.buttonLoadUriAsp.Size = new System.Drawing.Size(314, 38);
             this.buttonLoadUriAsp.TabIndex = 2;
             this.buttonLoadUriAsp.Text = "Load from URI (ASP.NET Generated)";
             this.buttonLoadUriAsp.UseVisualStyleBackColor = true;
@@ -101,10 +104,10 @@ namespace WindowsFormsExplorer.Samples.WorkbookView
             // buttonNewWorkbook
             // 
             this.buttonNewWorkbook.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.buttonNewWorkbook.Location = new System.Drawing.Point(12, 12);
-            this.buttonNewWorkbook.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.buttonNewWorkbook.Location = new System.Drawing.Point(17, 20);
+            this.buttonNewWorkbook.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
             this.buttonNewWorkbook.Name = "buttonNewWorkbook";
-            this.buttonNewWorkbook.Size = new System.Drawing.Size(220, 25);
+            this.buttonNewWorkbook.Size = new System.Drawing.Size(314, 42);
             this.buttonNewWorkbook.TabIndex = 1;
             this.buttonNewWorkbook.Text = "New Workbook";
             this.buttonNewWorkbook.UseVisualStyleBackColor = true;
@@ -112,9 +115,10 @@ namespace WindowsFormsExplorer.Samples.WorkbookView
             // 
             // buttonLoadDisk
             // 
-            this.buttonLoadDisk.Location = new System.Drawing.Point(12, 52);
+            this.buttonLoadDisk.Location = new System.Drawing.Point(17, 87);
+            this.buttonLoadDisk.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.buttonLoadDisk.Name = "buttonLoadDisk";
-            this.buttonLoadDisk.Size = new System.Drawing.Size(220, 23);
+            this.buttonLoadDisk.Size = new System.Drawing.Size(314, 38);
             this.buttonLoadDisk.TabIndex = 0;
             this.buttonLoadDisk.Text = "Load from Disk...";
             this.buttonLoadDisk.UseVisualStyleBackColor = true;
@@ -127,20 +131,21 @@ namespace WindowsFormsExplorer.Samples.WorkbookView
             | System.Windows.Forms.AnchorStyles.Right)));
             this.panel2.Controls.Add(this.workbookView);
             this.panel2.Controls.Add(this.panel3);
-            this.panel2.Location = new System.Drawing.Point(254, 3);
+            this.panel2.Location = new System.Drawing.Point(362, 5);
+            this.panel2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(294, 317);
+            this.panel2.Size = new System.Drawing.Size(421, 528);
             this.panel2.TabIndex = 3;
             // 
             // workbookView
             // 
             this.workbookView.Dock = System.Windows.Forms.DockStyle.Fill;
             this.workbookView.FormulaBar = null;
-            this.workbookView.Location = new System.Drawing.Point(0, 47);
+            this.workbookView.Location = new System.Drawing.Point(0, 78);
+            this.workbookView.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.workbookView.Name = "workbookView";
-            this.workbookView.Size = new System.Drawing.Size(294, 270);
+            this.workbookView.Size = new System.Drawing.Size(421, 450);
             this.workbookView.TabIndex = 1;
-            this.workbookView.Visible = true;
             this.workbookView.WorkbookSetState = resources.GetString("workbookView.WorkbookSetState");
             // 
             // panel3
@@ -150,15 +155,17 @@ namespace WindowsFormsExplorer.Samples.WorkbookView
             this.panel3.Controls.Add(this.label1);
             this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(294, 47);
+            this.panel3.Size = new System.Drawing.Size(421, 78);
             this.panel3.TabIndex = 0;
             // 
             // progressBar
             // 
-            this.progressBar.Location = new System.Drawing.Point(12, 11);
+            this.progressBar.Location = new System.Drawing.Point(17, 18);
+            this.progressBar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.progressBar.Name = "progressBar";
-            this.progressBar.Size = new System.Drawing.Size(423, 26);
+            this.progressBar.Size = new System.Drawing.Size(604, 43);
             this.progressBar.Style = System.Windows.Forms.ProgressBarStyle.Marquee;
             this.progressBar.TabIndex = 2;
             this.progressBar.Visible = false;
@@ -166,31 +173,35 @@ namespace WindowsFormsExplorer.Samples.WorkbookView
             // locationLabel
             // 
             this.locationLabel.AutoSize = true;
-            this.locationLabel.Location = new System.Drawing.Point(175, 17);
+            this.locationLabel.Location = new System.Drawing.Point(250, 28);
+            this.locationLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.locationLabel.Name = "locationLabel";
-            this.locationLabel.Size = new System.Drawing.Size(0, 15);
+            this.locationLabel.Size = new System.Drawing.Size(740, 25);
             this.locationLabel.TabIndex = 1;
+            this.locationLabel.Text = "Select one of the buttons to create or open a workbook and load it into the Workb" +
+    "ookView.";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(12, 17);
+            this.label1.Location = new System.Drawing.Point(17, 28);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(157, 15);
+            this.label1.Size = new System.Drawing.Size(238, 25);
             this.label1.TabIndex = 0;
             this.label1.Text = "Current Workbook Source:";
             // 
             // ActiveWorkbookSample
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.Controls.Add(this.tableLayoutPanel1);
-            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
             this.Name = "ActiveWorkbookSample";
-            this.Size = new System.Drawing.Size(551, 323);
+            this.Size = new System.Drawing.Size(787, 538);
             this.tableLayoutPanel1.ResumeLayout(false);
             this.panel1.ResumeLayout(false);
             this.panel2.ResumeLayout(false);
